25c8840c4f9a8c14e1849a5dd92696d5526ee873
Michele Rawlins <shellbop@att.net>
2020-01-25 10:10:49 -0600

    added alot

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ console.log("What's up");
 
 const pets = [
     {
-      imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\calico kitten.jpg",
-       name: "Cheetah",
+      imageUrl:"./pics/calico kitten.jpg",
+      name: "Cheetah",
       color: "Calico",
       specialSkill: "Meows for food and climbs trees",
       type: "Cat",
     },
 
     {
-        imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\purple dino.jpg",
+        imageUrl:"./pics/roxie.jpg",
         name: "Roxie",
         color: "Tan",
         specialSkill: "Catches tail and wraps herself arounds bushes",
@@ -18,42 +18,42 @@ const pets = [
       },
 
       {
-        imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\white kitty.jpg",
+        imageUrl:"./pics/white kitty.jpg",
          name: "Tundra",
         color: "White",
         specialSkill: "Climbs on your back and holds meetings for catnip addiction",
         type: "Cat",
       },
       {
-        imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\fancy dino.jpg",
+        imageUrl:"./pics/fancy dino.jpg",
          name: "Tiny",
         color: "Green",
         specialSkill: "Reformed Vegetarian who enjoys a juicy burger",
         type: "Dino",
       },
       {
-        imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\green dino.jpg",
+        imageUrl:"./pics/green dino.jpg",
         name: "Sweet Pea",
         color: "Green",
         specialSkill: "Plays soccer and bakes cookies",
         type: "Dino",
       },
       {
-        imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\magic kitty.jpg",
+        imageUrl:"./pics/magic kitty.jpg",
         name: "Fluffy",
         color: "Black",
         specialSkill: "It's magical and likes catnip.",
         type: "Cat",
       },
       {
-        imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\lassie.jpg",
+        imageUrl:"./pics/lassie.jpg",
         name: "Lassie",
         color: "Multiple",
         specialSkill: "Finds Timmy down the well",
         type: "Dog",
       },
       {
-        imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\purple dino.jpg",
+        imageUrl:"./pics/purple dino.jpg",
         name: "Dougie",
         color: "Purple",
         specialSkill: "Sings love songs in the shower",
@@ -61,7 +61,7 @@ const pets = [
       },
 
 {
-      imageUrl:"C:\Users\ydall\workspace\foundations\exercises\pet-adoption2\pics\pink dog.jpg",
+      imageUrl:"./pics/pink dog.jpg",
       name: "Rover",
       color: "Pink",
       specialSkill: "Plays guitar and wears a mohawk",
@@ -72,25 +72,28 @@ const pets = [
 
 console.log(pets);
 
-
+const printToDom = (divId,textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+ selectedDiv.innerHTML = textToPrint;
+ }
 
 const petCards = () => {
 
 let domString='';
 for(let i = 0; i <pets.length; i++){
-   domString += `<div class="pets">`;
+  domString += `<div class ="card">`;
+   domString += `<div class="adoption">`;
    domString += `<h1>${pets[i].name}</h1>`;
-   domString += `<img class = "img" src="${pets[i].imageURL}">`;
+   domString += `<img class="img" src="${pets[i].imageUrl}">`;
    domString += `<h3><br>${pets[i].color}</h3>`;
    domString += `<h3>${pets[i].specialSkill}</h3>`;
-   domString += `<button>${pets[i].type}</button>`;
+   domString += `<button class = "button">${pets[i].type}</button>`;
    domString +=`</div>`;
+   domString += `</div>`;
 }
-    const printToDom = (divId,textToPrint) => {
-     const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = textToPrint;
-    }
-    printToDom('mypets',domString );
+
+printToDom('mypets',domString );
+ 
 };
 
 petCards();
