de4e77959cc64504d6153fa7dfb422e79cfa031b
Michele Rawlins <shellbop@att.net>
2020-01-26 13:24:48 -0600

    sunday

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const pets = [
     {
       imageUrl:"./pics/calico kitten.jpg",
       name: "Cheetah",
-      color: "Calico",
+      color: "Pet color: Calico",
       specialSkill: "Meows for food and climbs trees",
       type: "Cat",
     },
@@ -12,7 +12,7 @@ const pets = [
     {
         imageUrl:"./pics/roxie.jpg",
         name: "Roxie",
-        color: "Tan",
+        color: "Petcolor: Tan",
         specialSkill: "Catches tail and wraps herself arounds bushes",
         type: "Dog",
       },
@@ -20,42 +20,42 @@ const pets = [
       {
         imageUrl:"./pics/white kitty.jpg",
          name: "Tundra",
-        color: "White",
+        color: "Pet color: White",
         specialSkill: "Climbs on your back and holds meetings for catnip addiction",
         type: "Cat",
       },
       {
         imageUrl:"./pics/fancy dino.jpg",
          name: "Tiny",
-        color: "Green",
+        color: "Pet color: Green",
         specialSkill: "Reformed Vegetarian who enjoys a juicy burger",
         type: "Dino",
       },
       {
         imageUrl:"./pics/green dino.jpg",
         name: "Sweet Pea",
-        color: "Green",
+        color: "Pet color: Green",
         specialSkill: "Plays soccer and bakes cookies",
         type: "Dino",
       },
       {
         imageUrl:"./pics/magic kitty.jpg",
         name: "Fluffy",
-        color: "Black",
+        color: "Pet color: Black",
         specialSkill: "It's magical and likes catnip.",
         type: "Cat",
       },
       {
         imageUrl:"./pics/lassie.jpg",
         name: "Lassie",
-        color: "Multiple",
+        color: "Pet color: Multiple",
         specialSkill: "Finds Timmy down the well",
         type: "Dog",
       },
       {
         imageUrl:"./pics/purple dino.jpg",
         name: "Dougie",
-        color: "Purple",
+        color: "Pet color: Purple",
         specialSkill: "Sings love songs in the shower",
         type: "Dino",
       },
@@ -63,7 +63,7 @@ const pets = [
 {
       imageUrl:"./pics/pink dog.jpg",
       name: "Rover",
-      color: "Pink",
+      color: "Pet color: Pink",
       specialSkill: "Plays guitar and wears a mohawk",
       type: "Dog",
     }
@@ -89,7 +89,7 @@ const displayCard = (event) => {
       domString += `<img class="img" src="${pets[i].imageUrl}">`;
       domString += `<h3><br>${pets[i].color}</h3>`;
       domString += `<h3>${pets[i].specialSkill}</h3>`;
-      domString += `<button class = "button">${pets[i].type}</button>`;
+      domString += `<button>${pets[i].type}</button>`;
       domString +=`</div>`;
       domString += `</div>`;
     }
@@ -113,7 +113,7 @@ const petCards = () => {
     domString += `<img class="img" src="${pets[i].imageUrl}">`;
     domString += `<h3><br>${pets[i].color}</h3>`;
     domString += `<h3>${pets[i].specialSkill}</h3>`;
-    domString += `<button class = "button">${pets[i].type}</button>`;
+    domString += `<button>${pets[i].type}</button>`;
     domString +=`</div>`;
     domString += `</div>`;
   }
